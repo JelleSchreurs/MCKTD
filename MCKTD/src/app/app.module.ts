a7ff9b2a13d66fce6c1d49d4abf2f89c0f4b7d81
Jelle Schreurs <jspuntdesign@hotmail.com>
2019-10-14 12:49:46 +0200

    changes

--- MCKTD/src/app/app.module.ts
+++ MCKTD/src/app/app.module.ts
@@ -3,21 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoadingComponent } from './loading/loading.component';
-<<<<<<< HEAD
 import { StartPageComponent } from './start-page/start-page.component';
-=======
-import { AlertComponent } from './alert/alert.component';
->>>>>>> 586bd913557d782e0199e265971436830d2d43af
 
 @NgModule({
   declarations: [
     AppComponent,
     LoadingComponent,
-<<<<<<< HEAD
     StartPageComponent
-=======
-    AlertComponent
->>>>>>> 586bd913557d782e0199e265971436830d2d43af
   ],
   imports: [
     BrowserModule
