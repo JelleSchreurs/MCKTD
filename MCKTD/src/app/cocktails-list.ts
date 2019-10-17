405fe9cd0b977b9d0ad862143c73f957cd83facc
HoussemEddine <houssem_gharbi@outlook.com>
2019-10-17 14:59:52 +0200

    cocktails

--- MCKTD/src/app/cocktails-list.ts
+++ MCKTD/src/app/cocktails-list.ts
@@ -1,7 +1,7 @@
 import { Cocktail } from './cocktails-module';
 
 export const COCKTAILS: Cocktail[] = [
-    { name: 'Martini', receipt: 'this is my reciept' },
+    { name: 'Martini1', receipt: 'this is my reciept' },
     { name: 'Martini2', receipt: 'this is my reciept' },
     { name: 'Martini3', receipt: 'this is my reciept' },
     { name: 'Martini4', receipt: 'this is my reciept' },
