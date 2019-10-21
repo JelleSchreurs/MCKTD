906899bea71a624a201a260ba1b3b4f1103197cd
HoussemEddine <houssem_gharbi@outlook.com>
2019-10-21 11:45:00 +0200

    loading component deleted

--- MCKTD/src/app/app.module.ts
+++ MCKTD/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing-module';
 import { AppComponent } from './app.component';
-import { LoadingComponent } from './loading/loading.component';
 import { StartPageComponent } from './start-page/start-page.component';
 import { LoginComponent } from './login/login.component';
 import { CocktailsComponent } from './cocktails/cocktails.component';
@@ -21,7 +20,6 @@ import { HomeComponent } from './home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoadingComponent,
     StartPageComponent,
     LoginComponent,
     CocktailsComponent,
