2f4af67d3ac1c8fbdcb3c28de075589ff1445c4c
Jelle Schreurs <jspuntdesign@hotmail.com>
2019-10-23 14:54:19 +0200

    first load mocktailDb

--- MCKTD/src/app/_class/mocktail-list.ts
+++ MCKTD/src/app/_class/mocktail-list.ts
@@ -1,6 +1,8 @@
 import { Mocktail } from './mocktails-module';
 
 export const MOCKTAILS: Mocktail[] = [
-    { id: 1, name: 'mocktailOne', recipe: 'this is my mocktail recipe', garnish: '', imageUrl: '' },
-    { id: 2, name: 'mocktailTwo', recipe: 'this is my mocktail recipe', garnish: '', imageUrl: '' }
+    // tslint:disable-next-line: max-line-length
+    { id: 1, name: '4th of July Firecracker Mocktail', recipe: '1️ Fill a tall glass with ice️⃣ Add in non-alcoholic pina colada mix.Slowly pour a mix of cherry up and grenadine (better red color and great tasting with the pina colada mix!) Slowly add your blue option. I used blue kool-aide.Top with whipped cream and sprinkles.', garnish: '', imageUrl: '../../assets/Images/4thOfJulyFirecrackerMocktail.jpg' },
+    // tslint:disable-next-line: max-line-length
+    { id: 2, name: 'Muddled Lemon', recipe: '¼ of a Lime ¼ of a Lemon ¼ oz / 10ml - Pepperberry Syrup Handful of Mint Top with Soda Muddle the mint, Lime, Lemon and Syrup together in a shaker. Add ice and shake.', garnish: '', imageUrl: '../../assets/Images/MuddledLemon.jpg' }
 ];
