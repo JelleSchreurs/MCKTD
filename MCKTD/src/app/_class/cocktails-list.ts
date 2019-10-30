eb441d623a89fe97a68c842c4a35720e518dbe82
Jelle Schreurs <jspuntdesign@hotmail.com>
2019-10-30 10:06:46 +0100

    restyling profile page

--- MCKTD/src/app/_class/cocktails-list.ts
+++ MCKTD/src/app/_class/cocktails-list.ts
@@ -2,45 +2,60 @@ import { Cocktail } from './cocktails-module';
 
 export const COCKTAILS: Cocktail[] = [
     // tslint:disable-next-line: max-line-length
-    { id: 1, name: 'Bangkok Denpasar', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/BangkokDenpasar.jpg' },
+    { id: 1, name: 'Bangkok Denpasar', recipe: 'Vodka infused with Butterfly Pea Tea 45 ml, Home made Cinnamon/Red apple syrup 15, Lime juice 15, Clarified Apple fresh 45', garnish: 'Cinnamon Stick & Dry Apple', imageUrl: '../../assets/Images/BangkokDenpasar.jpg' },
     // tslint:disable-next-line: max-line-length
-    { id: 2, name: 'Bitter Orange Mule', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/BitterOrangeMule.jpg' },
+    { id: 2, name: 'Bitter Orange Mule', recipe: '⁣⁠2oz Dehydrated Orange Infused Vodka, .5oz Carrot Juice, .25oz Egg White, 4oz Ginger Beer', garnish: 'Dehydrated Orange Peel and Mint⁣⁠', imageUrl: '../../assets/Images/BitterOrangeMule.jpg' },
     // tslint:disable-next-line: max-line-length
-    { id: 3, name: 'Blue Birds Fly To London', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/BlueBirdsFlyToLondon.jpg' },
-    { id: 4, name: 'ButterFly', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/Butterfly.jpg' },
+    { id: 3, name: 'Blue Birds Fly To London', recipe: '60ml The London N1 Gin, 15ml Blue Curacao, 10ml Crème de Violette, 20ml Lemon Juice, 10ml Simple', garnish: 'Crushed ice and Dirty dump topping', imageUrl: '../../assets/Images/BlueBirdsFlyToLondon.jpg' },
     // tslint:disable-next-line: max-line-length
-    { id: 5, name: 'Coconut White Rasta', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/CoconutWhiteRasta.jpg' },
+    { id: 4, name: 'ButterFly', recipe: '40ml Gin Infused with Buttefly Pea Tea, 20ml Lychee Liqueur, 15ml Fresh lime⁠, 4 dashes Perfect Foam', garnish: 'free of choice', imageUrl: '../../assets/Images/Butterfly.jpg' },
     // tslint:disable-next-line: max-line-length
-    { id: 6, name: 'Coniferous Fores', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/ConiferousForest.jpg' },
-    { id: 7, name: 'Cover Club Twist', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/CoverClubTwist.jpg' },
+    { id: 5, name: 'Coconut White Rasta', recipe: 'Coconut Rum, Agave nectar, Pimento Dram, Coconut half & half - 50/50 coco milk & cream', garnish: 'Coconut, Cinnamon', imageUrl: '../../assets/Images/CoconutWhiteRasta.jpg' },
     // tslint:disable-next-line: max-line-length
-    { id: 8, name: 'Crimson & Clover Club', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/CrimsonAndCloverClub.jpg' },
-    { id: 9, name: 'Diamond Spritz', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/DiamondSpritz.jpg' },
-    { id: 10, name: 'Fille de Berlin', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/FilleDeBerlin⁠.jpg' },
-    { id: 11, name: 'Gin Ginger Daisy', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/GinGingerDaisy.jpg' },
-    { id: 12, name: 'Gin Sour Twist', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/GinSourTwist.jpg' },
+    { id: 6, name: 'Coniferous Fores', recipe: '60 ml Beluga Hanting Bitter, 20 ml Martini Bianco, 20 ml Fresh lime, 20 ml cone syrup, 2 dash angostura bitters', garnish: 'Smoked Lime', imageUrl: '../../assets/Images/ConiferousForest.jpg' },
     // tslint:disable-next-line: max-line-length
-    { id: 13, name: 'Harvey WallBanger Twist', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/HarveyWallbangerTwist.jpg' },
-    { id: 14, name: 'Ichigo Rosé', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/IchigoRose.jpg' },
+    { id: 7, name: 'Cover Club Twist', recipe: 'NouasisonGin, June Gin Liquer⁠, Merengue, Raspberries, Maltesers, Rose, Kefir⁠', garnish: 'Chocolate cookie', imageUrl: '../../assets/Images/CoverClubTwist.jpg' },
     // tslint:disable-next-line: max-line-length
-    { id: 15, name: 'Mostly a Northern Song', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/MostlyANorthernSong⁣⁠.jpg' },
-    { id: 16, name: 'Mr Shulze\'s Sour', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/MrShulzeSour.jpg' },
-    { id: 17, name: 'N/A', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/NA.jpg' },
+    { id: 8, name: 'Crimson & Clover Club', recipe: 'Earl Grey Gin, Aperitif Wine,House-made Raspberry Jam, Grenadine, Lemon, Raspberry Dust', garnish: 'Raspberry', imageUrl: '../../assets/Images/CrimsonAndCloverClub.jpg' },
+    // tslint:disable-next-line: max-line-length
+    { id: 9, name: 'Diamond Spritz', recipe: 'Belvedere Vodka,Galliano L\’aperitivo, Sassy Cider, Aperol, Veuve Clicquot Rich Champagne, Gosnell\’s Hopped Mead', garnish: 'Thyme', imageUrl: '../../assets/Images/DiamondSpritz.jpg' },
+    // tslint:disable-next-line: max-line-length
+    { id: 10, name: 'Fille de Berlin', recipe: 'Sipsmith Gin⁠, Creme De Cassis⁠, Fentimen\'s Rose Lemonade⁠, Fresh Lime⁠, Musk Syrup⁠', garnish: 'Pink flower leaf', imageUrl: '../../assets/Images/FilleDeBerlin⁠.jpg' },
+    // tslint:disable-next-line: max-line-length
+    { id: 11, name: 'Gin Ginger Daisy', recipe: '60ml Rutte Old Simon Genever, 25ml Lemon Juice, 10ml Maraschino, 10ml Gum Syrup, 10ml Fresh Ginger Juice, Top with Soda Water', garnish: 'Dryed Vanilla leaves, Olives', imageUrl: '../../assets/Images/GinGingerDaisy.jpg' },
+    // tslint:disable-next-line: max-line-length
+    { id: 12, name: 'Gin Sour Twist', recipe: '60ml Lemon Infused Rutte Celery Gin, 30ml Chambord, 15ml Cherry Syrup⁠, 25ml Lemon Juice⁠, 1 egg white⁠', garnish: 'Dried lime powder', imageUrl: '../../assets/Images/GinSourTwist.jpg' },
+    // tslint:disable-next-line: max-line-length
+    { id: 13, name: 'Harvey WallBanger Twist', recipe: '45ml Vodka Infused with Mango⁠, 15ml Galliano Vanilla⁠, 65ml Fresh Orange Juice⁠, 15ml Lemon Acid⁠', garnish: 'Orange, Lemon', imageUrl: '../../assets/Images/HarveyWallbangerTwist.jpg' },
+    // tslint:disable-next-line: max-line-length
+    { id: 14, name: 'Ichigo Rose', recipe: 'Strawberry-infused Dampfwerk gin⁠, House infused rosé vermouth with rose hip, rose petals & hibiscus, House orange bitters ⠀', garnish: 'Strawberry', imageUrl: '../../assets/Images/IchigoRose.jpg' },
+    // tslint:disable-next-line: max-line-length
+    { id: 15, name: 'Mostly a Northern Song', recipe: '1 oz @aalborg_akvavit Taffel⁠; 1 oz aviation gin⁠, 3/4 oz Amaro Braulio⁣⁠, 3/4 oz grapefruit juice⁣⁠, 1/2 oz lemon juice⁣⁠, 1/2 oz simple syrup⁣⁠, tiny pinch salt', garnish: 'rye cracker, smoked salmon, dill', imageUrl: '../../assets/Images/MostlyANorthernSong⁣⁠.jpg' },
+    // tslint:disable-next-line: max-line-length
+    { id: 16, name: 'Mr Shulze\'s Sour', recipe: 'Dry Gin⁠, Absinthe⁠, Lemon⁠, Egg White⁠, Bloody Shiraz⁠', garnish: 'Dryed cristal sugar', imageUrl: '../../assets/Images/MrShulzeSour.jpg' },
+    // tslint:disable-next-line: max-line-length
+    { id: 17, name: 'N/A', recipe: 'Cognac⁠, Scotch⁠, Sweet Vermouth⁠, Cocoa Nib⁠, Couple of dashes of mystery⁠', garnish: 'On the Rocks', imageUrl: '../../assets/Images/NA.jpg' },
     { id: 18, name: 'New York Sour', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/NewYorkSour.jpg' },
     // tslint:disable-next-line: max-line-length
     { id: 19, name: 'Nordic Mule', recipe: '1.5 oz aquavit .75 oz ginger liqueur .75 oz lime juice Top with equal parts cucumber soda (Mr. Qcumber) and sparkling water. Shake first three ingredients with ice and strain into a tall glass filled with ice. Top with cucumber soda and sparking water. Garnish with cucumber and rosemary.', garnish: '/', imageUrl: '../../assets/Images/NordicMule.jpg' },
-    { id: 20, name: 'Odyssee', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/Odyssee.jpg' },
+    // tslint:disable-next-line: max-line-length
+    { id: 20, name: 'Odyssee', recipe: '2oz Rye whiskey or bourbon, 0.75oz Simple Syrup, 1oz Lemon juice - 1 Egg white (optional), 0.5 oz Red wine', garnish: 'Orange, Grape', imageUrl: '../../assets/Images/Odyssee.jpg' },
     // tslint:disable-next-line: max-line-length
     { id: 21, name: 'Old Fashioned', recipe: 'Vodka infused with Butterfly Pea Tea 45 ml, Home made Cinnamon/Red apple syrup 15, Lime juice 15, Clarified Apple fresh 45', garnish: '/', imageUrl: '../../assets/Images/OldFashioned.jpg' },
-    { id: 22, name: 'Other Worldly', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/OtherWorldly.jpg' },
-    { id: 23, name: 'Passion Jungle', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/PassionJungle.jpg' },
-    { id: 24, name: 'Pink Cosmo', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/PinkCosmo.jpg' },
     // tslint:disable-next-line: max-line-length
-    { id: 25, name: 'Queen\'s Park Swizzle', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/QueenSParkSwizzle⁠.jpg' },
+    { id: 22, name: 'Other Worldly', recipe: '2 oz Hendricks Orbium Gin, .75 oz Elderflower Liqueur .5 oz fresh lime juice, Top off with Ginger Beer', garnish: 'Blackberry, Mint', imageUrl: '../../assets/Images/OtherWorldly.jpg' },
     // tslint:disable-next-line: max-line-length
-    { id: 26, name: 'Red Light District', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/RedLightDistrict.jpg' },
-    { id: 27, name: 'Risque Business', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/RisqueBusiness.jpg' },
-    { id: 28, name: 'Roku Sour', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/RokuSour.jpg' },
+    { id: 23, name: 'Passion Jungle', recipe: '1.5 oz aquavit .75 oz ginger liqueur, .75 oz lime juice. Top with equal parts cucumber soda,sparkling water. Shake first three ingredients with ice and strain into a tall glass filled with ice. Top with cucumber soda and sparking water.', garnish: 'Cucumber, Rosemary', imageUrl: '../../assets/Images/PassionJungle.jpg' },
+    // tslint:disable-next-line: max-line-length
+    { id: 24, name: 'Pink Cosmo', recipe: '45ml Cranberry Vodka 45 ml, 20ml Red orange liquor, 20ml Pink grapefruit cordial. Top Up London Essence Grapefruit Soda', garnish: 'Carambola', imageUrl: '../../assets/Images/PinkCosmo.jpg' },
+    // tslint:disable-next-line: max-line-length
+    { id: 25, name: 'Queen\'s Park Swizzle', recipe: 'Denizen Rum⁠, Demerara Syrup⁠, lime juice⁠, Mint leaves⁠, Angostura Bitters⁠', garnish: 'Mint', imageUrl: '../../assets/Images/QueenSParkSwizzle⁠.jpg' },
+    // tslint:disable-next-line: max-line-length
+    { id: 26, name: 'Red Light District', recipe: '45 ml Vodka, 30 ml Carménère wine, 25 ml beetroot & ginger syrup, 30 ml raspberry juice, 15 ml lemon juice', garnish: 'Blueberry, Strawberry, Orange, Carambolla', imageUrl: '../../assets/Images/RedLightDistrict.jpg' },
+    // tslint:disable-next-line: max-line-length
+    { id: 27, name: 'Risque Business', recipe: 'Leblon Cachaça⁠, Prickly Pear Syrup⁠, Coconut Cream⁠, Jalepeño Green Chartreuse⁠, Fresh Lime Juice⁠, served over crushed ice', garnish: 'Mint, Lime', imageUrl: '../../assets/Images/RisqueBusiness.jpg' },
+    // tslint:disable-next-line: max-line-length
+    { id: 28, name: 'Roku Sour', recipe: '50 ml Roku gin, 5 ml Creme de mente, 15 ml Yuzy syrup, 20 ml Fhesh lime, 2 dash angostura orange bitters, 20 ml egg white', garnish: 'Mint, Orange', imageUrl: '../../assets/Images/RokuSour.jpg' },
     // tslint:disable-next-line: max-line-length
     { id: 29, name: 'Soa Paulo Swizzle', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/SaoPauloSwizzle.jpg' },
     { id: 30, name: 'Signature Style', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/SignatureStyle.jpg' },
@@ -52,7 +67,7 @@ export const COCKTAILS: Cocktail[] = [
     { id: 34, name: 'Southpaw', recipe: 'Belvedere, Absenteroux, ’cool’, cordial', garnish: '/', imageUrl: '../../assets/Images/Southpaw.jpg' },
     { id: 35, name: 'Story Teller', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/StoryTeller.jpg' },
     // tslint:disable-next-line: max-line-length
-    { id: 36, name: 'The Georgetown Demerare Rum Slinger', recipe: 'this is my reciept', garnish: 'posh cucumber ribbon/viola flower trumpet thing.', imageUrl: '../../assets/Images/TheGeorgetownDemeraraRumSling⁠.jpg' },
+    { id: 36, name: 'The Georgetown Demerare Rum Slinger', recipe: '60ml 151-Proof Demerara Rum⁠, 30ml Cherry Heering, 30ml Lime Juice⁠, 1 Dash of Angostura Bitters⁠, 3 Drops of Benedictine ⁠', garnish: 'mint, olive', imageUrl: '../../assets/Images/TheGeorgetownDemeraraRumSling⁠.jpg' },
     { id: 37, name: 'Theo Broma', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/TheoBrama.jpg' },
     { id: 38, name: 'Tiki', recipe: 'this is my reciept', garnish: '/', imageUrl: '../../assets/Images/Tiki.jpg' },
     // tslint:disable-next-line: max-line-length
